7c5b309bd4eb78d25b4908fa5601b9a9d3d108f1
SALLY GONZALEZ <sallygonzalez1908@gmail.com>
2024-05-08 22:36:18 -0600

    Modificación de types interfaces

--- plp-products/src/types/product.ts
+++ plp-products/src/types/product.ts
@@ -1,8 +1,24 @@
 export interface Producto {
-    id: string,
-    nombre: string,
-    priceSelling: string,
-    priceList: string,
-    imagen?: string,
-    skus?: string[]
+  id: string,
+  nombre: string,
+  priceSelling: string,
+  priceList: string,
+  imagen?: string,
+  skus?: string[]
+}
+
+export interface ProductoList {
+  id: string,
+  nombre: string,
+  priceSelling: string,
+  priceList: string,
+  imagen?: string,
+  skus?: string[]
+}
+
+export interface ProductoItem {
+  id: string,
+  nombre: string,
+  precio: string,
+  cantidad: number
 }
