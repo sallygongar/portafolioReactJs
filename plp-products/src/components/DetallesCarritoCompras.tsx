f2eaa76f1ec5ad596d76ab1fff200fed004a6e0c
SALLY GONZALEZ <sallygonzalez1908@gmail.com>
2024-05-24 16:40:08 -0600

    Css en componente detalles de compra

--- plp-products/src/components/DetallesCarritoCompras.tsx
+++ plp-products/src/components/DetallesCarritoCompras.tsx
@@ -5,18 +5,35 @@ interface CarritoProps {
   carrito: any[],
   title: string
 }
-
+ 
 const DetallesCarritoCompras: React.FC<CarritoProps> = ({carrito,title}) => {
   return (
     <>
     <h2>{title}</h2>
-    <ul>
+    <div className="cart__list-content">
+      <div className="cart__list__container-productos">
       {carrito.map((item, index) => (
-        <li key={index}>
-         {item.cantidad} {item.nombre} - Talla {item.talla} -  $ {item.precioFinal}
-        </li>
-      ))}
-    </ul>
+        <div className="cart__list__wrapper_detalles" key={index}>
+        <div className="cart__list__wrapper_imagen">
+          <div className="cart__list_img">
+             <img src={item.imagen} alt={item.imagen} width="100%"/>
+          </div>
+        </div>
+        <div className="cart__list__wrapper_producto">
+          <div className="cart__list__caracteristicas">
+            <span>{item.nombre}</span>
+            <span>{item.talla}</span>
+            <span>{item.precioFinal}</span>
+          </div>
+        </div>
+      </div>
+       ))}
+      </div>
+      <div  className="cart__list__container-resumen">
+        <h1>Resumen de pgo</h1>
+      </div>
+    </div>
+    
   </>
   )
 }
